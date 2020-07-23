f2ea0fd986e36a583e1da1c95aacaa3b9f1b99dd
Sarah Hudspeth <shhudspeth@gmail.com>
2020-07-23 00:17:48 +0000

    completed Madlibs flask app form and story

--- madlibs.py
+++ madlibs.py
@@ -41,6 +41,38 @@ def greet_person():
                            person=player,
                            compliment=compliment)
 
+@app.route('/game')
+def show_madlib_form():
+    play_game = request.args.get("yes")
+    name = request.args.get('person')
+    compliment = request.args.get('compliment')
+
+    if play_game == "yes":
+        return render_template("game.html", name=name, compliment=compliment)
+    else:
+        return render_template("goodbye.html", name=name)
+
+@app.route("/madlibs")
+def show_madlib():
+    check_boxes = []
+    for key in request.args:
+         if str(key).startswith("plural_noun"):
+             check_boxes.append(request.args[key])
+   
+    madlib_choices = {}
+    madlib_choices['name'] = request.args.get("name")
+    madlib_choices['noun'] = request.args.get('noun')
+    madlib_choices['person'] = request.args.get('person')
+    madlib_choices['color'] = request.args.get('color')
+    madlib_choices['adj'] = request.args.get("adj")
+    madlib_choices['plural_nouns'] = check_boxes
+    madlib_choices['adverb'] = request.args.get('adverb')
+   
+    return render_template("madlibs.html",choices_dict=madlib_choices)
+
+
+
+
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
